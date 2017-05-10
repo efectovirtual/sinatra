c57d3edc91c9e7e5c168ddfd21990874aeb03faa
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-05-10 12:47:48 -0500

    Ejercicio Salúdame 1

---
+++ sinatra.rb
@@ -0,0 +1,18 @@
+
+
+
+
+# require 'sinatra'
+
+# a = []
+
+# get '/' do
+# 	a = params[:nombre]
+# 	if a
+# 		"<h3>Hola #{a}</h1>"
+# 	else
+# 		"Hola desconocido!"
+# 	end	
+# end
+
+
---
+++ solution.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+a = []
+
+get '/' do
+	a = params[:nombre]
+	if a
+		"<h3>Hola #{a}</h1>"
+	else
+		"Hola desconocido!"
+	end	
+end
+
+
