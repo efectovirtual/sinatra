f577a631a3931f411e33ab49fd8b73f9a41e4cb3
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-05-10 15:09:33 -0500

    Ejercicio El Código Secreto

--- solution.rb
+++ solution.rb
@@ -1,20 +1,5 @@
 require 'sinatra'
 
-a = []
-
 get '/' do
-	a = params[:nombre]
-	if a
-		"<h3>Hola #{a}</h1>"
-	else
-		"Hola desconocido!"
-	end	
-
-	erb :saludo3
-
-end
-
-
-post '/nuevo/objeto' do
-	"¡Hola #{params[:nombre]}!"
+	'HcUy6Re2LLBRtj'
 end
