73f2978a632a7528bdff92ccb19551508fa2e435
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-05-23 17:48:29 -0500

    Solution Par e Impar

--- solution.rb
+++ solution.rb
@@ -3,11 +3,18 @@ require 'sinatra'
 
 # Pares e Impares
 get '/' do
+
+	@parimpar = []
+	51.times do |i|
+		if (i.to_i%2) == 0
+			@parimpar << "#{i} Soy Par!"
+		else
+			@parimpar << "#{i} Soy Impar!"
+		end
+	end
+
   erb :parimpar
 end
-
-
-
 
 
 # require 'sinatra'
