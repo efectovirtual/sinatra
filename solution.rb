a6ef289bb52d2dbc84fbbbd0d848e8f13a51486e
efectovirtual <albertorodriguez@MacBook-Pro-de-Alberto.local>
2017-05-25 16:24:36 -0500

    sumar sinatra

--- solution.rb
+++ solution.rb
@@ -1,21 +1,48 @@
 require 'sinatra'
 
+suma = 0
 
-# Pares e Impares
 get '/' do
-
-	@parimpar = []
-	50.times do |i|
-		i += 1
-		if (i.to_i%2) == 0
-			@parimpar << "#{i} Soy Par!"
-		else
-			@parimpar << "#{i} Soy Impar!"
-		end
-	end
-
-  erb :parimpar
+	@suma_total = suma
+	erb :sumar
 end
+
+post '/' do
+	suma += 1
+	@suma_total = suma
+	erb :sumar
+end
+
+
+
+
+
+
+
+
+
+
+# # Pares e Impares
+# get '/' do
+
+# 	@parimpar = []
+# 	50.times do |i|
+# 		i += 1
+# 		if (i.to_i%2) == 0
+# 			@parimpar << "#{i} Soy Par!"
+# 		else
+# 			@parimpar << "#{i} Soy Impar!"
+# 		end
+# 	end
+
+#   erb :parimpar
+# end
+
+
+
+
+
+
 
 
 # require 'sinatra'
